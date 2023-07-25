d242449e1b8f91c2f0dfce346776d1b8bab664c7
Braulio <brauliorg12@gmail.com>
2023-07-25 03:34:01 -0300

    BUGFIX - Mejoras para PageSpeed, se elimina bootstrap local y se agrega el ultimo por cdn junto con otros js y css, se actualizan comandos de build, se unifica style.css comentado, varios cambios de html principal para soportar boostrap 5, refactor de codigo y mejoras de tipado, limpieza de codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,17 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('Fading', [
-      state('void', style({ opacity: 0 })),
-      state('*', style({ opacity: 0.6 })),
-      transition(':enter', animate('800ms ease-out')),
-      transition(':leave', animate('100ms ease-in')),
-    ]),
-  ],
 })
 export class AppComponent {
   title = 'CubaNova';
 
-  public element = false;
+  constructor() {}
 
-  constructor() {
-    window.addEventListener('scroll', this.scroll, true);
+  scrollPoint(section: string) {
+    // document.getElementById(section).scrollIntoView({behavior: "smooth"});
+    document.getElementById(section).scrollIntoView();
   }
-
-  scroll = (event): void => {
-    const sc = event.target.scrollingElement.scrollTop;
-    if (sc >= 300) {
-      this.element = true;
-    } else {
-      this.element = false;
-    }
-  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
